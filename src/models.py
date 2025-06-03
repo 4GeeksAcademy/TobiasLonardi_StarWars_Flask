3967bb3ba7fea05fde571dc1b047bb8a25ef39f3
TobiLonardi <123268672+TobiLonardi@users.noreply.github.com>
2025-06-03 21:29:47 +0000

    primer push

--- src/models.py
+++ src/models.py
@@ -1,19 +1,35 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, DateTime, func, Text, Table, Column,Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+import enum
 
 db = SQLAlchemy()
 
+class CharactersGenders(enum.Enum):
+    MALE = "male"
+    FEMALE = "female"
+    OTHER = "other"
+
+
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    __tablename__ = "user"
+    id:Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    username: Mapped[str] = mapped_column(String(80), nullable=False)
+    password: Mapped[str] = mapped_column(String(80), nullable=False)
+
+class Planet(db.Model):
+    __tablename__ = "planet"
+    id:Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(80), nullable=False)
+    description: Mapped[str] = mapped_column(String(160))
+    population: Mapped[int] = mapped_column(nullable=False)
+
+class Character(db.Model):
+    __tablename__ = "character"
+    id:Mapped[int] = mapped_column(primary_key=True)
+    gender:Mapped[enum] = mapped_column(Enum(CharactersGenders))
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+
+
