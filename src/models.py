8e2b196e1a329f30ea55528c2332eace44d78648
TobiLonardi <123268672+TobiLonardi@users.noreply.github.com>
2025-06-04 16:47:38 +0000

    creacion de personajes y planetas

--- src/models.py
+++ src/models.py
@@ -1,9 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean, ForeignKey, DateTime, func, Text, Table, Column,Enum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 import enum
 
 db = SQLAlchemy()
+
+class CharacterSide(enum.Enum):
+    LIGHTSIDE = "lightSide"
+    DARKSIDE = "darkSide"
+    NEUTRAL = "neutral"
 
 class CharactersGenders(enum.Enum):
     MALE = "male"
@@ -17,6 +23,7 @@ class User(db.Model):
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     username: Mapped[str] = mapped_column(String(80), nullable=False)
     password: Mapped[str] = mapped_column(String(80), nullable=False)
+    favorites_planets: Mapped["Favorites_Planets"] = relationship(back_populates="owner")
 
 class Planet(db.Model):
     __tablename__ = "planet"
@@ -28,7 +35,18 @@ class Planet(db.Model):
 class Character(db.Model):
     __tablename__ = "character"
     id:Mapped[int] = mapped_column(primary_key=True)
+    name:Mapped[str] = mapped_column(String(100),nullable=False)
     gender:Mapped[enum] = mapped_column(Enum(CharactersGenders))
+    side:Mapped[enum] = mapped_column(Enum(CharacterSide))
+
+class Favorites_Planets:
+    __tablename__ = "favorites_planets"
+    id:Mapped[int] = mapped_column(primary_key=True)
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planet.id"), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    owner: Mapped["User"] = relationship(back_populates="favorites_planets")
+
+
 
 
 
